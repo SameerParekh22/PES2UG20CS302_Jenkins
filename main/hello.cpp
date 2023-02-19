14e874f900cac036be246453ebe078ce1c9d45e3
Sameer Parekh <79531711+SameerParekh22@users.noreply.github.com>
2023-02-19 21:56:39 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Hello, this is Sameer Parekh PES2UG20CS302!";
   return 0;
 }
